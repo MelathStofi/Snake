d2b7ab233cdc1e11fe6b89bcb198b1985d91ebe1
Melath Stofi <melath.stofi@gmail.com>
2019-05-07 19:01:23 +0200

    Functions getch() add: reads keyboard; Function main() test-modify according to the getch()

---
+++ krixkrax.py
@@ -0,0 +1,79 @@
+'''from pynput import keyboard  # Ehhez installálni kell a pip3-at "sudo apt install python3-pip" kommanddal, utána "pip3 install pynput" kommand.
+
+# The currently active modifiers
+current = set()
+
+
+def on_press(key):
+    pass
+
+
+def on_release(key):
+    pass
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
+def execute():
+    print("kakk")'''
+
+
+
+'''
+import curses
+
+# get the curses screen window
+screen = curses.initscr()
+
+# turn off input echoing
+curses.noecho()
+
+# respond to keys immediately (don't wait for enter)
+curses.cbreak()
+
+# map arrow keys to special values
+screen.keypad(True)
+
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_RIGHT:
+            # print doesn't work with curses, use addstr instead
+            screen.addstr(0, 0, 'right')
+        elif char == curses.KEY_LEFT:
+            screen.addstr(0, 0, 'left ')
+        elif char == curses.KEY_UP:
+            screen.addstr(0, 0, 'up   ')
+        elif char == curses.KEY_DOWN:
+            screen.addstr(0, 0, 'down ')
+finally:
+    # shut down cleanly
+    curses.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
+'''
+
+
+def getch():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+while True:
+    x = getch()
+    if x == "w":
+        print("testing")
+    elif x == "s":
+        print
--- snake.py
+++ snake.py
@@ -2,6 +2,19 @@ import os
 import random
 
 
+def getch():
+    # Ez itt a billentyűzetfigyelő függvény!
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
 def generate_table(column, row):
     table = []
     for i in range(column):
@@ -26,7 +39,11 @@ def print_table(table):
     print(dash)
 
 
-def create_object(table):
+def print_snake(table):
+    pass
+
+
+def generate_object(table):
     rand_col_index = random.randint(0, (len(table)-1))
     rand_row_index = random.randint(0, (len(table[rand_col_index])-1))
     table[rand_col_index][rand_row_index] = "Ó"
@@ -34,9 +51,20 @@ def create_object(table):
 
 def main():
     board = generate_table(20, 30)
-    print_table(board)
-    create_object(board)
-    print_table(board)
+    while True:
+        generate_object(board)
+        print_table(board)
+        while True:
+            move = getch()
+            if move == "w":
+                print("testing")
+            elif move == "s":
+                print("testing")
+            elif move == "d":
+                print("testing")
+            elif move == "a":
+                print("testing")
+
 
 if __name__ == "__main__":
     main()
