7d8da871acaf42c532db6adbd9a98a27ccc4a5d8
Melath Stofi <melath.stofi@gmail.com>
2019-05-07 11:30:20 +0200

    Function generate_table modify: '0' changed to 'space'; Function print_table modify: printing changes according to the table's row

--- snake.py
+++ snake.py
@@ -1,20 +1,28 @@
 def generate_table(column, row):
     table = []
     for i in range(column):
-        table.append([0] * row)
+        table.append([" "] * row)
     return table
 
 
 def print_table(table):
+    dash = "--"
+    for row in table:
+        for elem in row:
+            dash += "-"
+        break
+
+    print(dash)
     for row in table:
         one_row = ""
         for elem in row:
             one_row += str(elem)
-        print(one_row)
+        print(f"|{one_row}|")
+    print(dash)
 
 
 def main():
-    print_table(generate_table(20, 20))
+    print_table(generate_table(20, 30))
 
 
 if __name__ == "__main__":
